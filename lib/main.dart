1cd6a0dae1bd95c3306dc1485fbf8f179541ff01
Murilo-Baron <muuhbarrow@gmail.com>
2023-07-14 09:36:22 -0300

    problemas de código dart desatualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/material.dart';
-//TODO: Step 2 - Import the rFlutter_Alert package here.
-import 'package:rflutter_alert/rflutter_alert.dart';
-import 'QuizBrain.dart';
+import 'package:flutter/material.dart'; // Importe o pacote rflutter_alert
 
-QuizBrain quizBrain = QuizBrain();
+import 'quiz_brain.dart';
 
 void main() => runApp(Quizzler());
 
@@ -35,47 +32,6 @@ class _QuizPageState extends State<QuizPage> {
   void checkAnswer(bool userPickedAnswer) {
     bool correctAnswer = quizBrain.getCorrectAnswer();
 
-    setState(() {
-      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If so,
-      //On the next line, you can also use if (quizBrain.isFinished()) {}, it does the same thing.
-      if (quizBrain.isFinished() == true) {
-        //TODO Step 4 Part A - show an alert using rFlutter_alert,
-
-        //This is the code for the basic alert from the docs for rFlutter Alert:
-        //Alert(context: context, title: "RFLUTTER", desc: "Flutter is awesome.").show();
-
-        //Modified for our purposes:
-        Alert(
-          context: context,
-          title: 'Finished!',
-          desc: 'You\'ve reached the end of the quiz.',
-        ).show();
-
-        //TODO Step 4 Part C - reset the questionNumber,
-        quizBrain.reset();
-
-        //TODO Step 4 Part D - empty out the scoreKeeper.
-        scoreKeeper = [];
-      }
-
-      //TODO: Step 6 - If we've not reached the end, ELSE do the answer checking steps below 👇
-      else {
-        if (userPickedAnswer == correctAnswer) {
-          scoreKeeper.add(Icon(
-            Icons.check,
-            color: Colors.green,
-          ));
-        } else {
-          scoreKeeper.add(Icon(
-            Icons.close,
-            color: Colors.red,
-          ));
-        }
-        quizBrain.nextQuestion();
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -101,8 +57,8 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
+            child: TextButton(
+              textcolor: Colors.white,
               color: Colors.green,
               child: Text(
                 'True',
@@ -112,7 +68,6 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
                 checkAnswer(true);
               },
             ),
@@ -121,7 +76,7 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
+            child: TextButton(
               color: Colors.red,
               child: Text(
                 'False',
@@ -131,7 +86,6 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
                 checkAnswer(false);
               },
             ),
@@ -145,8 +99,9 @@ class _QuizPageState extends State<QuizPage> {
   }
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
